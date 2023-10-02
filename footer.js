bc09241b0a256d354fa10cd40106cd1e569a9efd
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2023-10-02 13:31:37 +0700

    update footer

--- footer.js
+++ footer.js
@@ -4,22 +4,29 @@ let footer_location = document.querySelectorAll(".footer_location");
 
 let footer_map_title = document.querySelector(".footer_details_left h2");
 let footer_copy_right = document.querySelector(".copy_right");
+let footer_office_taiwan = document.getElementById("taiwan_location");
+let factory2 = document.getElementById("factory2");
 let footer_eng = {
   footer_brand_name:
-    "TUONG LAN AGRICULTURAL AND FOODSTUFFS PROCESSING EXPORT COMPANY LIMITED (TUONG LAN AGREX., LTD)",
+    "TUONG LAN AGRICULTURAL AND FOODSTUFFS  PROCESSING EXPORT COMPANY LIMITED (TUONG LAN AGREX CO., LTD)",
   footer_location:
-    "TRAI TRUNG VILLAGE, YEN MY TOWN, YEN MY DISTRICT, HUNG YEN PROVINCE",
-  footer_office: `<b>Hanoi Office: </b> NO. 49, LE DAI THANH STREET, HAI BA DISTRICT
-    TRUNG. HANOI`,
-  footer_map_title: "MAP",
+    "<b>HEADQUARTER: </b> TRAI TRANG COMMUNE, YEN MY DISTRICT, HUNG YEN PROVINCE, VIET NAM",
+  footer_office: `<b>HANOI OFFICE: </b> NO. 49, LE DAI HANH STREET, LE DAI HANH WARD HAI BA TRUNG DISTRICT, HANOI, VIETNAM`,
+  factory2:
+    "<b>FACTORY NO.2: </b> NO. 68, NATIONAL HIGHWAY 80, THANH PHU HAMLET, TAN BINH COMMUNE, CHAU THANH, DONG THAP PROVINCE, VIETNAM",
+  footer_office_taiwan:
+    "<b>TAIWAN OFFICE: </b>NO.7, LANE 65 GUOYONG ROAD, FENGSHAN DISTRICT, KAOHSIUNG CITY, TAIWAN",
+  // footer_map_title: "MAP",
   footer_copy_right:
     "COPYRIGHT BELONGS TO TUONG LAN ARGEX CO., LTD | SUPPLIED BY SAPO",
 };
 
 if (page_language == "ENG") {
   footer_brand_name.innerText = footer_eng.footer_brand_name;
-  footer_location[0].innerText = footer_eng.footer_location;
+  footer_location[0].innerHTML = footer_eng.footer_location;
   footer_location[2].innerHTML = footer_eng.footer_office;
-  footer_map_title.innerText = footer_eng.footer_map_title;
+  // footer_map_title.innerText = footer_eng.footer_map_title;
   footer_copy_right.innerText = footer_eng.footer_copy_right;
+  footer_office_taiwan.innerHTML = footer_eng.footer_office_taiwan;
+  factory2.innerHTML = footer_eng.factory2;
 }
